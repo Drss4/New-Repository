2c0cad8f8d416c5be63454725d3fdc0f5b17a6e8
Zunyang Wang <zwang17@georgefox.edu>
2018-04-13 01:12:32 -0700

    This is Button D again

--- ATM.java
+++ ATM.java
@@ -2,3 +2,4 @@
         private static final int ERROR_STATE = 6;
                 _dButton = new JButton(" D ");
                 _dButton.addActionListener(new DButtonListener());
+                buttonPanel.add(_dButton);

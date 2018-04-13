1ba64938d254cc2bba471b6620e5a6d11d1829e5
Zunyang Wang <zwang17@georgefox.edu>
2018-04-13 01:12:19 -0700

    Added new button D

--- ATM.java
+++ ATM.java
@@ -1,2 +1,4 @@
         private static final int FASTCASH_STATE = 5;
         private static final int ERROR_STATE = 6;
+                _dButton = new JButton(" D ");
+                _dButton.addActionListener(new DButtonListener());
